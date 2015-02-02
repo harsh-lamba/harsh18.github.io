eb9e3d5e6f54a18f854853e5a279a50cd39c4910
harsh18 <harsh.lamba181189@gmail.com>
2015-02-02 14:56:18 +0530

    Form

--- app/pages/login/loginController.js
+++ app/pages/login/loginController.js
@@ -9,10 +9,13 @@
 angular.module('hisabKitabLoginController', []).
 	controller('loginController', ['$scope', function($scope){
 		var userSecureData = {};
+		$scope.formSubmitted = false;
 		$scope.submitLoginForm = function(){
+			//turning formsubmission true
+			$scope.formSubmitted = true;
 			userSecureData = $scope.userSecureData;
 			console.log($scope.userSecureData);
 			//Access service to store data into DB....(for now it is Session storage).
-			
+
 		};
 	}]);
